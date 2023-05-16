6ea507ff79dfb90f3cd4ab322b537b7503f48dbd
CaffeinatedJitterBug <itwallaby@gmail.com>
2023-05-15 20:05:52 -0400

    Fixed fileOCR function and added a for loop that find the total on the receipt by searching for the biggest number

--- assets/script.js
+++ assets/script.js
@@ -71,19 +71,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
 receiptSubmit.addEventListener('click', fileOCR);
 
-function fileOCR(event) {
+async function fileOCR(event) {
   event.preventDefault();
   const ocrKey = 'K86624004988957';
   const fileName = document.getElementById('receipt').files[0];
-  const fileURL = URL.createObjectURL(fileName);
-  const ocrURL = 'https://api.ocr.space/parse/imageUrl?apikey=' + ocrKey + '&url=' + fileURL + '&filetype=jgp';
+  const ocrURL = 'https://api.ocr.space/parse/image';
+  const formData  = new FormData();
 
-  fetch(ocrURL)
+  formData.append('apikey', ocrKey);
+  formData.append('file', fileName);
+
+  fetch(ocrURL, {
+    method: 'POST',
+    body: formData
+  })
     .then(function(response) {
       return response.json();
     })
     .then(function(data) {
-      console.log(data);
+      const nums = data.ParsedResults[0].ParsedText
+        .split('\r\n')
+        .map(function(line) { return parseFloat(line) })
+        .filter(function(line) { return !isNaN(line) });
+      let biggestNum = 0;
+
+      for (let x=0; x<nums.length; x++) {
+        if (biggestNum < nums[x]) {
+          biggestNum = nums[x];
+        }
+      }
     })
 }
 //End AG
